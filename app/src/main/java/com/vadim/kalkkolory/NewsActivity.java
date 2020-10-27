c401bdc7b3cfc5235be0bae86c31b53b52e3272d
Vadim <gabderahmanov99@gmail.com>
2020-10-27 18:15:49 +0400

    add img, update NewsActivity

--- app/src/main/java/com/vadim/kalkkolory/NewsActivity.java
+++ app/src/main/java/com/vadim/kalkkolory/NewsActivity.java
@@ -2,13 +2,44 @@ package com.vadim.kalkkolory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.squareup.picasso.Picasso;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private ImageView mainimg;
+    private TextView description;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    private String url;
+    private String urlToImg;
+    private String getTitle;
+    private String getDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        mainimg = (ImageView) findViewById(R.id.app_bar_image);
+        description = (TextView) findViewById(R.id.descriptionNews);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar);
+
+        Intent intent = getIntent();
+
+        url = intent.getStringExtra("url");
+        urlToImg = intent.getStringExtra("urtImg");
+        getTitle = intent.getStringExtra("title");
+        getDescription = intent.getStringExtra("content");
+
+        Picasso.get().load(urlToImg).error(R.drawable.not_found).into(mainimg);
+        collapsingToolbarLayout.setTitle(getTitle);
+        description.setText(getDescription);
+
     }
 }
